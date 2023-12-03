0e778a35024180d075adf05aab3ed4f5ae0ad821
SE13 <se13codeur@gmail.com>
2023-12-03 14:20:26 +0100

    added checkWhitelist for authorize

--- Whitelist.sol
+++ Whitelist.sol
@@ -6,4 +6,28 @@ contract Whitelist {
   mapping(address => bool) public whitelistedMap;
 
   event Authorized(address _address);
+  event EthReceived(address _from, uint256 _value);
+
+
+
+  receive() external payable {
+    emit EthReceived(msg.sender, msg.value);
+  }
+
+  fallback() external payable {
+    emit EthReceived(msg.sender, msg.value);
+  }
+
+  function checkWhitelist() private view returns(bool) {
+    if (whitelistedMap[msg.sender]) {
+      return true;
+    }
+    return false;
+  }
+
+  function authorize(address _address) public {
+    require(checkWhitelist(), "You are not authorized");
+    whitelistedMap[_address] = true;
+    emit Authorized(_address);
+  }
 }
